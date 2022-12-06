9be35fbf9276c14f17d1fb44b34d5aa8de51cbd9
Ananda Bayu <anandabayu090@gmail.com>
2022-12-06 09:00:44 +0700

    Update index.tsx

--- src/components/General/Header/index.tsx
+++ src/components/General/Header/index.tsx
@@ -78,7 +78,7 @@ const Header: React.FC = () => {
 					<Typography.Title
 						level={4}
 						style={style.title}
-						className="pt-[15px] max-md:pt-[5px]"
+						className="pt-[15px] max-md:pt-[10px]"
 					>
 						Covid Tracker
 					</Typography.Title>
